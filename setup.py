5208c3b8c8e0c1f22bca8797b42ebc2dcc29854f
Bansari Maru <bansari.galoria@gmail.com>
2026-03-18 14:44:36 +0100

    project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Bansari Maru",
+    author_email="bansari.gmaru@gmail.com",
+    packages=find_packages()
+)
